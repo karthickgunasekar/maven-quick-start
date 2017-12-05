e2776546920300111f89cefaf70cc1a6ef308017
karthickgunasekar <569gkarthi@gmail.com>
2017-12-05 21:42:57 +0530

    Adding the pom.xml with 1.8 as source and adding collection to Application.java

--- src/main/java/com/services/Application.java
+++ src/main/java/com/services/Application.java
@@ -1,13 +1,26 @@
 package com.services;
 import java.lang.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
+
+	public void greet() {
+		List<String> list = new ArrayList<>();
+		list.add("Hello");
+
+		for (String strVal : list) {
+			System.out.println("Greetings "+strVal);	
+		}
+	}
 
 	public Application() {
 		System.out.println("Application default Constructor");
 	}
 
 	public static void main(String[] args) {
-		Application application = new Application();		
+		System.out.println("Starting Application");
+		Application application = new Application();	
+		application.greet();	
 	}	
 }
