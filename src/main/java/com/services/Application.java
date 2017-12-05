576645da9a38ae714feab102e4e94213ff30f1f4
karthickgunasekar <569gkarthi@gmail.com>
2017-12-05 21:18:51 +0530

    Adding Application.java into project folder

---
+++ src/main/java/com/services/Application.java
@@ -0,0 +1,13 @@
+package com.services;
+import java.lang.*;
+
+public class Application {
+
+	public Application() {
+		System.out.println("Application default Constructor");
+	}
+
+	public static void main(String[] args) {
+		Application application = new Application();		
+	}	
+}
